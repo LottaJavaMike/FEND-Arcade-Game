11d807db438a4799058e4fef0f37face3a7d931d
Mike Shea <emtshea@gmail.com>
2018-08-27 09:19:24 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// Enemies our player must avoid
+var Enemy = function() {
+    // Variables applied to instances
+    this.x = x;
+    this.y = y;
+    this.speed = 150;
+    this.sprite = 'images/enemy-bug.png';
+};
+// Updates the enemy's position
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    this.x = (dt * this.speed) + this.x;
+    if (player.x > this.x + 100 && player.x > this.x - 50){
+      if (player.y > this.y - 55 && player.y + 50){
+        player.reset();
+      }
+    }
+//returns the player to start if off the screen
+    if (this.x > 600){
+      this.x = (-400) * Math.random();
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
